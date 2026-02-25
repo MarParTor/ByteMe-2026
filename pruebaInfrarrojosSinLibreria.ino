8ebd63509f19b7927602e32e4ed9df9427881bed
Marcos Pardo <105744591+MarParTor@users.noreply.github.com>
2026-02-25 10:48:11 +0100

    Create pruebaInfrarrojosSinLibreria.ino

---
+++ pruebaInfrarrojosSinLibreria.ino
@@ -0,0 +1,20 @@
+// Pines analógicos donde conectas OUT0...OUT7
+const int sensorPins[8] = {
+  A0, A1, A2, A3, A4, A5, A6, A7
+};
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  for (int i = 0; i < 8; i++) {
+    int value = analogRead(sensorPins[i]); // Lee el ADC
+    Serial.print("Sensor ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(value);
+  }
+  Serial.println();
+  delay(100); // Ajusta al ritmo que necesites
+}
